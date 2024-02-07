8b70f9dce9c1a91618e60686445bb4dadd636c6c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-07 21:19:45 +0200

    feat: Implement token generation and cookie setting

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '15d',
+  });
+
+  res.cookie('jwt', token, {
+    //   for security reasons
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15days 24hours 60min 60s 1000ms => MS
+    httpOnly: true, // the user will not access this cookie via JavaScript. Prevent XSS attacks cross-site scripting attacks
+    sameSite: 'strict', // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== 'development',
+  });
+};
+
+export default generateTokenAndSetCookie;
