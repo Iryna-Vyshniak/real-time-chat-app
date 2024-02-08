389338a1004c526bcf4b4f034a588d8ef9567159
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-08 21:59:37 +0200

    feat: Implement message controller

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,58 @@
+import { ctrlWrapper } from '../decorators/index.js';
+import Conversation from '../models/conversation.model.js';
+import Message from '../models/message.model.js';
+
+export const sendMessage = async (req, res) => {
+  const { id: receiverId } = req.params;
+  const { message } = req.body;
+  const senderId = req.user._id;
+
+  let conversation = await Conversation.findOne({
+    participants: { $all: [senderId, receiverId] },
+  });
+
+  if (!conversation) {
+    conversation = await Conversation.create({ participants: [senderId, receiverId] });
+  }
+
+  const newMessage = await Message.create({
+    senderId,
+    receiverId,
+    message,
+  });
+
+  if (newMessage) {
+    conversation.messages.push(newMessage._id);
+  }
+
+  //   await conversation.save();
+  //   await newMessage.save();
+
+  // this will run in parallel
+  await Promise.all([conversation.save(), newMessage.save()]);
+
+  res.status(201).json({ newMessage, message: 'Message sent successfully' });
+};
+
+// GET MESSAGES
+
+export const getMessages = async (req, res) => {
+  const { id: userToChatId } = req.params;
+  const senderId = req.user._id;
+
+  // get actual messages
+  const conversation = await Conversation.findOne({
+    participants: { $all: [senderId, userToChatId] },
+  }).populate('messages');
+
+  if (!conversation) return res.status(200).json([]);
+
+  const messages = conversation.messages;
+
+  res.status(200).json(messages);
+};
+
+export default {
+  getMessages: ctrlWrapper(getMessages),
+  sendMessage: ctrlWrapper(sendMessage),
+};
