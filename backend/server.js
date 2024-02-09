d825a27b965b2dd40ced268f3da428fef9555c5e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-09 12:21:13 +0200

    feat: Implement users router, serving static files and route fallback in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/api/auth.routes.js';
 import messageRouter from './routes/api/message.routes.js';
+import usersRouter from './routes/api/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +21,13 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
+app.use('/api/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 app.use((req, res) => {
   res.statusCode(404).json({ message: 'Not found' });
