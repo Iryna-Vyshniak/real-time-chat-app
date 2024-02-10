53d429bc4acf9fa216c64b7f8deaaefbd2f60f70
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-10 20:56:35 +0200

    feat: Add background images and login form fields data

---
+++ frontend/src/data/index.js
@@ -0,0 +1,48 @@
+export const bgImages = [
+  {
+    id: 'hjwehruwrj',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707569448/chat/carpathian.jpg',
+  },
+  {
+    id: 'hjweh4645yrtg',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707569334/chat/sunflower.jpg',
+  },
+  {
+    id: 'hjweh2342ddq215',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707570437/chat/alley.jpg',
+  },
+  {
+    id: 'hjwerdscsfset433',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707571843/chat/cliff.jpg',
+  },
+  {
+    id: 'hjwe3121323refe',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707571958/chat/vineyard.jpg',
+  },
+  {
+    id: 'hjwe31tertdve4',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707588419/chat/augustine.jpg',
+  },
+  {
+    id: 'hjwe3rfeftertdve4',
+    img: 'https://res.cloudinary.com/dkqxaid79/image/upload/v1707588572/chat/abstract.jpg',
+  },
+];
+
+// for login form
+export const loginFields = {
+  username: {
+    label: 'Username',
+    name: 'username',
+    type: 'text',
+    required: true,
+    placeholder: 'Enter username',
+  },
+  password: {
+    label: 'Password',
+    name: 'password',
+    type: 'text',
+    required: true,
+    placeholder: 'Enter Password',
+  },
+};
