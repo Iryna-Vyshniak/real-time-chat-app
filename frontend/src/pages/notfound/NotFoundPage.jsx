317bc4a5a07bf8d56e7d93d39c02e6f1823e2ba7
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-12 00:19:49 +0200

    feat: Implemented NotFoundPage component

---
+++ frontend/src/pages/notfound/NotFoundPage.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div className='flex justify-center items-center p-6 w-full rounded-lg shadow-md bg-slate-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <div className='text-center text-slate-300 tracking-wider'>
+        <h1 className='text-4xl font-medium'>404</h1>
+        <p className='text-xl font-medium m-6'>
+          Sorry, the page {"you're"} looking for {"can't"} be found
+        </p>
+        <NavLink to='/' className='bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded'>
+          Go Home
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
