073e7b927429a671d9c2da4d6dbf670661d35288
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-11 07:55:00 +0200

    style: added styles

--- frontend/src/components/ui/TextField.jsx
+++ frontend/src/components/ui/TextField.jsx
@@ -2,7 +2,9 @@ const TextField = ({ label, id, ...props }) => {
   return (
     <>
       <label htmlFor={id} className='label p-2'>
-        <span className='text-base label-text text-slate-300'>{label}</span>
+        <span className='text-base label-text text-slate-300 drop-shadow-[0px_0.5px_0.5px_rgba(0,0,0,1)]'>
+          {label}
+        </span>
       </label>
       <input id={id} className='w-full input input-bordered h-10' {...props} />
     </>
