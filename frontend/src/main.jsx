cce2dc45d38d0e593df23481b752a81f47cff8d8
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-12 21:42:05 +0200

    feat: Implemented auth context provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
 
 import './index.css';
 
+import { AuthContextProvider } from './shared/context/AuthContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
