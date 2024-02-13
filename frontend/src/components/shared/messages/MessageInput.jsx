af88210c2559ec15fc6d60ddfed938d26755e49b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-14 00:20:05 +0200

    feat: Implement useSendMessage hook for sending messages,  message sending functionality in messageInput component

--- frontend/src/components/shared/messages/MessageInput.jsx
+++ frontend/src/components/shared/messages/MessageInput.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react';
 
 import TextField from '../../ui/TextField';
 import Icon from '../../ui/Icon';
+import { useSendMessage } from '../../../shared/hooks/useSendMessage';
 
 const MessageInput = () => {
   const [message, setMessage] = useState('');
+  const { isLoading, sendMessages } = useSendMessage();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessages(message);
+    setMessage('');
+  };
+
   return (
-    <form className='px-4 mt-auto'>
+    <form className='px-4 mt-auto' onSubmit={handleSubmit}>
       <div className='relative w-full'>
         <TextField
           type='text'
@@ -14,11 +24,14 @@ const MessageInput = () => {
           name='message'
           value={message}
           onChange={({ target }) => setMessage(target.value)}
-          className='p-2.5 w-full bg-slate-500 border border-slate-800 text-sm tracking-wider rounded-lg  text-white'
+          className='p-2.5 w-full bg-beige border border-slate-800 text-sm tracking-wider rounded-lg  text-slate-900'
         />
         <button type='submit' className='absolute top-[45%] right-0 flex items-center pe-3'>
-          <div className='loading loading-spinner'></div>
-          <Icon src='#icon-paper-plane' />
+          {isLoading ? (
+            <span className='loading loading-spinner'></span>
+          ) : (
+            <Icon src='#icon-paper-plane' />
+          )}
         </button>
       </div>
     </form>
---
+++ frontend/src/shared/hooks/useSendMessage.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import useConversation from '../../store/useConversation';
+import toast from 'react-hot-toast';
+
+export const useSendMessage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  const sendMessages = async (message) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) throw new Error(data.error);
+
+      setMessages([...messages, data]);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { isLoading, sendMessages };
+};
