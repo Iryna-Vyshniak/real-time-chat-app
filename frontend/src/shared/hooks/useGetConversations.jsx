d465c4844292f025cbc80b0fe9ce7611cc635bec
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-13 22:53:51 +0200

    feat: Implement useGetConversations hook to fetch conversations

---
+++ frontend/src/shared/hooks/useGetConversations.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export const useGetConversations = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`/api/users`);
+        const data = await res.json();
+
+        if (data.error || data.message) {
+          throw new Error(data.error || data.message);
+        }
+
+        setConversations(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getConversations();
+  }, []);
+
+  return { isLoading, conversations };
+};
