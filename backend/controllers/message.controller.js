9d5558e758b6f75dc8ea5e0feaed1dc76e2572d5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-16 01:51:16 +0200

    Refactor: Simplify response data in POST message endpoint, improve clarity in GET messages endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -31,18 +31,18 @@ export const sendMessage = async (req, res) => {
   // this will run in parallel
   await Promise.all([conversation.save(), newMessage.save()]);
 
-  res.status(201).json({ newMessage, message: 'Message sent successfully' });
+  res.status(201).json(newMessage);
 };
 
 // GET MESSAGES
 
 export const getMessages = async (req, res) => {
-  const { id: userToChatId } = req.params;
-  const senderId = req.user._id;
+  const { id: receiverId } = req.params; // my receiver
+  const senderId = req.user._id; // it`s me
 
   // get actual messages
   const conversation = await Conversation.findOne({
-    participants: { $all: [senderId, userToChatId] },
+    participants: { $all: [senderId, receiverId] },
   }).populate('messages');
 
   if (!conversation) return res.status(200).json([]);
