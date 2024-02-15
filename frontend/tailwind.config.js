41c5ba2bfdd12515c9abac7eef6dc407cefd4c2a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-15 23:40:35 +0200

    chore: Implemented colors for tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#ea580c',
+        primary: '#D4EC8C',
+        secondary: '#FFD998',
+        green: '#B6D285',
+        beige: '#fee0ab',
+      },
+    },
   },
+
   backgroundImage: {},
   // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
