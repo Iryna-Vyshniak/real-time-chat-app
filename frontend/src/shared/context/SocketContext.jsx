f9e1c66fcc6bb4e6a051f62b0cd07812f3e5337c
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-17 21:12:52 +0200

    refactor: Socket url updated

--- frontend/src/shared/context/SocketContext.jsx
+++ frontend/src/shared/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:5000', {
+      const socket = io('https://chat-mern-ujj2.onrender.com', {
         query: {
           userId: authUser._id,
         },
