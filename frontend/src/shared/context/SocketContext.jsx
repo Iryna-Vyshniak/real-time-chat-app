01a1aabb1b85c2429ae0c303a09510f59f7e3b53
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-17 20:36:31 +0200

    feat: Implement SocketContextProvider to manage Socket.IO connection and online users list in the application.

---
+++ frontend/src/shared/context/SocketContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+import { useAuthContext } from './AuthContext';
+
+const SocketContext = createContext();
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io('http://localhost:5000', {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+
+      // socket.on() is used to listen to the events; can be used both on client and server side
+      socket.on('getOnlineUsers', (users) => {
+        setOnlineUsers(users);
+      });
+
+      () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>
+  );
+};
