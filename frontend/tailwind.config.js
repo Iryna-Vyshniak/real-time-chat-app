5beb5dc3a55f1cca533e342e9785ea53058da7e3
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-17 16:54:20 +0200

    style: Extend dropShadow utility in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,17 @@ export default {
         green: '#B6D285',
         beige: '#fee0ab',
       },
+      dropShadow: {
+        '1xl-white': '0 1px 1px rgba(250, 250, 250, 1)',
+        '1xl-black': '0 1px 0.5px rgba(0, 0, 0, 1)',
+        '2xl-black': '0 0.2px 0.2px rgba(0, 0, 0, 1)',
+        '2xl-red': '0 0.2px 0.2px rgba(250, 0, 0, 1)',
+        '2xl-double': ['0 0.2px 0.2px rgba(250, 0, 0, 1)', '0 0.2px 0.2px rgba(0, 0, 0, 1)'],
+        '3xl-black': '0 0.3px 0.3px rgba(0, 0, 0, 1)',
+        '3xl-red': '0 0.3px 0.3px rgba(250, 0, 0, 1)',
+        '5xl-black': '0 0.5px 0.5px rgba(0, 0, 0, 1)',
+        '5xl-red': '0 0.5px 0.5px rgba(250, 0, 0, 1)',
+      },
     },
   },
 
