1e6d23f2451118f60f68920f37e0fde716b01882
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-18 20:27:47 +0200

    update cors and favicon

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH'],
   },
 });
