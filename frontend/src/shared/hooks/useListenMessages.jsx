25d53bb1627f58020ac2b4306a81c21bc965a48d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-20 19:58:06 +0200

    refactor: Update message listener hook to handle notifications for new messages in different conversations.

--- frontend/src/shared/hooks/useListenMessages.jsx
+++ frontend/src/shared/hooks/useListenMessages.jsx
@@ -7,16 +7,23 @@ import messageSound from '../../assets/sounds/message-sound.mp3';
 
 export const useListenMessages = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages } = useConversation();
+  const { messages, setMessages, selectedConversation, notification, setNotification } =
+    useConversation();
 
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(messageSound);
-      sound.play();
-      setMessages([...messages, newMessage]);
+      if (!selectedConversation || selectedConversation._id !== newMessage.sender._id) {
+        if (!notification.includes(newMessage._id)) {
+          setNotification([...notification, newMessage]);
+        }
+      } else {
+        newMessage.shouldShake = true;
+        const sound = new Audio(messageSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      }
     });
 
     return () => socket?.off('newMessage');
-  }, [messages, setMessages, socket]);
+  }, [messages, notification, selectedConversation, setMessages, setNotification, socket]);
 };
