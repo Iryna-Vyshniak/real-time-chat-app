65b38e2a69d1b6b3d2b5b6c95ab217d5ef458674
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-20 19:50:18 +0200

    refactor: Extend conversation schema with additional fields

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -4,6 +4,9 @@ import { addUpdateSettings, handleMongooseError } from '../helpers/index.js';
 
 const conversationSchema = new Schema(
   {
+    chatName: { type: String, trim: true, default: 'New group' },
+    isGroupChat: { type: Boolean, default: false },
+    groupAdmin: { type: Schema.Types.ObjectId, ref: 'User' },
     participants: [
       {
         type: Schema.Types.ObjectId,
