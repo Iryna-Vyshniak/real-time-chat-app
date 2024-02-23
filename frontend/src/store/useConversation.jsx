3589a94d683190031282251fcc0fee867256c3da
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-23 19:28:05 +0200

    feat: Add conversationId state and setter to Zustand store

--- frontend/src/store/useConversation.jsx
+++ frontend/src/store/useConversation.jsx
@@ -2,13 +2,15 @@ import { create } from 'zustand';
 
 const useConversation = create((set) => ({
   selectedConversation: null,
-  setSelectedConversation: (selectedConversation) => set(() => ({ selectedConversation })),
+  setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
   messages: [],
-  setMessages: (messages) => set(() => ({ messages })),
+  setMessages: (messages) => set({ messages }),
   lastMessages: [],
-  setLastMessages: (lastMessages) => set(() => ({ lastMessages })),
+  setLastMessages: (lastMessages) => set({ lastMessages }),
   notification: [],
-  setNotification: (notification) => set(() => ({ notification })),
+  setNotification: (notification) => set({ notification }),
+  conversationId: null,
+  setConversationId: (conversationId) => set({ conversationId }),
 }));
 
 export default useConversation;
