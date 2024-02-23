0875e3a9cfe62c2aaedb356898a8a3d46e30bfda
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-23 19:25:44 +0200

    feat: Implement socket event to mark messages as read and emit confirmation event

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,7 @@
 import { Server } from 'socket.io';
 import http from 'http'; // it`s from node.js
 import express from 'express';
+import Message from '../models/message.model.js';
 
 const app = express();
 
@@ -27,6 +28,16 @@ io.on('connection', (socket) => {
 
   // io.emit() is used to send events to all the connected clients - for ex: who is online or offline
   io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+  socket.on('markMessagesAsRead', async ({ conversationId, userId }) => {
+    try {
+      await Message.updateMany({ conversationId, read: false }, { read: true }, { new: true });
+
+      io.to(userSocketMap[userId]).emit('messagesRead', { conversationId });
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
   // socket.on() is used to listen to the events; can be used both on client and server side
   socket.on('disconnect', () => {
