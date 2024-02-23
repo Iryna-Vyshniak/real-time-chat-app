49957e62d12e126dcad2c47d04201e32804cb35e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-23 19:29:39 +0200

    refactor: Enhance useGetMessages hook to include conversationId state and setter

--- frontend/src/shared/hooks/useGetMessages.jsx
+++ frontend/src/shared/hooks/useGetMessages.jsx
@@ -2,19 +2,33 @@ import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 
 import useConversation from '../../store/useConversation';
+import { getUserConversationId } from '../utils';
 
 export const useGetMessages = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const { messages, setMessages, selectedConversation, lastMessages } = useConversation();
+  const {
+    messages,
+    setMessages,
+    selectedConversation,
+    lastMessages,
+    setConversationId,
+    conversationId,
+  } = useConversation();
 
   useEffect(() => {
     const getMessages = async () => {
       setIsLoading(true);
       try {
+        if (!selectedConversation?._id) return;
+
         const res = await fetch(`/api/messages/${selectedConversation._id}`);
         const data = await res.json();
 
         if (data.error) throw new Error(data.error);
+
+        // getting the conversation ID for a particular user and setting that ID as the active conversation
+        const conversationsId = getUserConversationId(data, selectedConversation._id);
+        setConversationId(conversationsId);
 
         setMessages(data);
       } catch (error) {
@@ -23,8 +37,8 @@ export const useGetMessages = () => {
         setIsLoading(false);
       }
     };
-    if (selectedConversation?._id) getMessages();
-  }, [selectedConversation._id, setMessages, lastMessages]);
+    getMessages();
+  }, [selectedConversation?._id, setMessages, lastMessages, setConversationId]);
 
-  return { messages, isLoading };
+  return { messages, isLoading, conversationId };
 };
