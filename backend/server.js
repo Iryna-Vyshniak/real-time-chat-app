79887e2227e3f181efa3530d670a25aa967304f5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-24 04:53:52 +0200

    feat: Configure Cloudinary and setup form data parsing in server configuration

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from 'cloudinary';
 
 import authRouter from './routes/api/auth.routes.js';
 import messageRouter from './routes/api/message.routes.js';
@@ -16,7 +17,14 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.urlencoded({ extended: true })); // to parse form data (from req.body)
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
