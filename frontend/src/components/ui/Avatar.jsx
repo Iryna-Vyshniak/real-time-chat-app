60318b0009884fbb2ede2be0dd04ac957d3fe9b8
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-24 04:49:26 +0200

    refactor: update UI design, added props style

--- frontend/src/components/ui/Avatar.jsx
+++ frontend/src/components/ui/Avatar.jsx
@@ -1,14 +1,24 @@
-const Avatar = ({ src, selected, isOnline }) => {
+const Avatar = ({ src, selected, isOnline, style }) => {
   return (
     <div
       tabIndex={0}
       role='button'
-      className={`btn btn-ghost btn-circle avatar shadow-xl shadow-primary/30 
+      className={
+        !style
+          ? `btn btn-ghost btn-circle avatar shadow-xl shadow-primary/30 
       ${isOnline ? 'online' : ''} 
-      ${selected ? 'ring ring-accent ring-offset-base-100 ring-offset-2' : ''}`}
+      ${selected ? 'ring ring-accent ring-offset-base-100 ring-offset-2' : ''}`
+          : 'update-avatar btn btn-ghost btn-circle avatar shadow-xl shadow-primary/30'
+      }
     >
-      <div className='w-10 rounded-full'>
-        <img alt='user avatar' src={src} width='2.8rem' height='2.8rem' className='shadow-lg' />
+      <div className={style ? style : 'w-10 rounded-full'}>
+        <img
+          alt='user avatar'
+          src={src}
+          width='2.8rem'
+          height='2.8rem'
+          className={'w-[2.8rem] h-[2.8rem] shadow-lg'}
+        />
       </div>
     </div>
   );
