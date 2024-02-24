ba54d9410875f7a7d2aaa8a459018afc5a2a36bb
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-25 00:18:49 +0200

    fix: Add process.exit(1) to terminate the process upon MongoDB connection error

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 const connectToMongoDB = async () => {
   try {
     await mongoose.connect(process.env.DB_HOST);
+
     console.log('Database connection successful');
   } catch (error) {
     console.log('Error connecting to MongoDB', error.message);
+    process.exit(1);
   }
 };
 
