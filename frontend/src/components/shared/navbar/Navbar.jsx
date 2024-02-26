b31677eedd5b19c77efd5679c7fa5b4ce3a0958b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-02-27 01:05:38 +0200

    feat: Add navigation to clear selected conversation

--- frontend/src/components/shared/navbar/Navbar.jsx
+++ frontend/src/components/shared/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import Navigation from './Navigation';
+import useConversation from '../../../store/useConversation';
 
 const Navbar = () => {
+  const { setSelectedConversation } = useConversation();
+
   return (
     <header className='fixed left-0 top-0 z-10 w-full h-[4.5rem] bg-slate-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <div className='container flex items-center justify-between'>
-        <Logo />
+        <Link to='/' onClick={() => setSelectedConversation(null)}>
+          {' '}
+          <Logo />
+        </Link>
 
         <Navigation />
       </div>
