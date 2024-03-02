68eb610a96b9288764cb3205918abd21fd5cd2bf
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-02 11:47:04 +0200

    style: test

--- frontend/src/components/shared/sidebar/Sidebar.jsx
+++ frontend/src/components/shared/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from './SearchInput';
 
 const Sidebar = () => {
   return (
-    <div className='grid grid-rows-[auto,auto,1fr] h-[auto] gap-1 p-4 max-w-full rounded-lg bg-green/10 brightness-105 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1 text-slate-300 md:border-r border-slate-300'>
+    <div className='grid grid-rows-[auto,auto,1fr] gap-1 p-4 max-w-full rounded-lg bg-green/10 brightness-105 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1 text-slate-300 md:border-r border-slate-300'>
       <SearchInput />
       <Divider style='hidden md:block' />
       <Conversations />
