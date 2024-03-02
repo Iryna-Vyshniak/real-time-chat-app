19018c14709d0cc88166826e3a65d4a4c88de73e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-02 11:12:27 +0200

    style: test  dynamic height viewport for mobile gadgeds

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
   return (
     <div className='flex flex-col justify-center h-screen overflow-hidden'>
       <Navbar />
-      <main className='relative flex-1 flex items-center justify-center mt-[4.5rem]'>
+      <main className='relative flex-1 flex items-center justify-center mt-[5.5rem]'>
         {' '}
         <Suspense
           fallback={
