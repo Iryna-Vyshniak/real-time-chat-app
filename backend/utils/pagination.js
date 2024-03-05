b9baa84885e70ad5ef9fe978dca65d8aeb32cb56
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-05 21:11:46 +0200

    feat: Implement pagination function with default values

---
+++ backend/utils/pagination.js
@@ -0,0 +1,10 @@
+export const pagination = (persistPage = 1, persistLimit = 6) => {
+  const parsePage = parseInt(persistPage);
+  const parseLimit = parseInt(persistLimit);
+
+  const page = parsePage >= 1 ? parsePage : 1;
+  const limit = parseLimit > 1 && parseLimit < 6 ? parseLimit : 6;
+  const skip = (page - 1) * limit;
+
+  return { page, skip, limit };
+};
