74d1ea6487e75b57835302a4b767bd5816dff7a8
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-06 23:58:51 +0200

    feat: Add useMemo hook to properly generate emoji

--- frontend/src/components/shared/messages/NoChatSelected.jsx
+++ frontend/src/components/shared/messages/NoChatSelected.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useAuthContext } from '../../../shared/context/AuthContext.jsx';
 import { generateEmoji } from '../../../shared/utils/index.js';
 import Icon from '../../ui/Icon';
@@ -5,11 +6,16 @@ import Icon from '../../ui/Icon';
 const NoChatSelected = () => {
   const { authUser } = useAuthContext();
 
+  // generate emoji
+  const generateConversationsWithEmoji = useMemo(() => {
+    return generateEmoji();
+  }, []);
+
   return (
     <div className='grid place-items-center gap-4 px-4 sm:text-lg md:text-xl text-accent font-semibold text-center tracking-wider drop-shadow-2xl-double'>
       <p>Welcome 👋</p>
       <h1>
-        {authUser.fullName} <span>{generateEmoji()}</span>
+        {authUser.fullName} <span>{generateConversationsWithEmoji}</span>
       </h1>
       <p className='text-center'>Select a chat to start messaging</p>
       <Icon src='#icon-chat' style='w-8 h-8' />
