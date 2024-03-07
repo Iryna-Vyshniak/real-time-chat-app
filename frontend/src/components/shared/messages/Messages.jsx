3c10f62b997f8483f4eda0fe7fd6400e7b433132
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-07 20:53:16 +0200

    Refactor: Introduce loaded messages handling

--- frontend/src/components/shared/messages/Messages.jsx
+++ frontend/src/components/shared/messages/Messages.jsx
@@ -1,78 +1,61 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useGetMessages } from '../../../shared/hooks/useGetMessages';
 import { useListenMessages } from '../../../shared/hooks/useListenMessages';
 import useConversation from '../../../store/useConversation';
 
-import MessageSkeleton from '../skeletons/MessageSkeleton';
 import Message from './Message';
+import LoadMoreMessages from './LoadMoreMessages';
 
 const Messages = () => {
-  const { selectedConversation, currentPage } = useConversation();
-
-  const { isLoading, messages, loadMoreMessages } = useGetMessages();
+  const [lastMessageElement, setLastMessageElement] = useState(null);
 
   useListenMessages();
 
-  const lastMessageRef = useRef(null);
+  const { selectedConversation, currentPage, messages } = useConversation();
+  console.log('currentPage: ', currentPage);
 
+  const { isLoading } = useGetMessages();
   // scroll to the last messsages
   useEffect(() => {
-    const element = lastMessageRef.current;
-
-    const handleScroll = () => {
-      const { scrollTop, clientHeight, scrollHeight } = lastMessageRef.current;
-      if (scrollTop + clientHeight >= scrollHeight) {
-        loadMoreMessages(currentPage + 1);
-      }
-    };
-
-    if (element) {
-      element.addEventListener('scroll', handleScroll);
+    if (!isLoading && messages.length > 0) {
+      lastMessageElement?.scrollIntoView({ behavior: 'smooth' });
     }
-
-    return () => {
-      if (element) {
-        element.removeEventListener('scroll', handleScroll);
-      }
-    };
-  }, [loadMoreMessages, currentPage, isLoading]);
-
-  useEffect(() => {
-    if (!isLoading) {
-      const timerId = setTimeout(() => {
-        lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
-        loadMoreMessages(currentPage + 1);
-      }, 1000);
-      return () => clearTimeout(timerId);
-    }
-  }, [currentPage, isLoading, loadMoreMessages]);
+  }, [messages.length]);
 
   return (
     <div className='mb-4 px-4 flex-auto w-full overflow-auto touch-auto will-change-scroll'>
-      {!isLoading && messages.length > 0 && (
-        <ul className='flex-1 px-4 overflow-auto'>
-          {messages.map((message) => {
-            if (
-              message.receiver._id === selectedConversation._id ||
-              message.sender._id === selectedConversation._id
-            ) {
-              return (
-                <li key={message._id} ref={lastMessageRef}>
-                  <Message message={message} />
-                </li>
-              );
-            }
-          })}
-        </ul>
-      )}
-      {/* if loading - 4 cycles display skeleton */}
-      {isLoading && [...Array(4)].map((_, idx) => <MessageSkeleton key={idx} />)}
-      {!isLoading && messages.length === 0 && (
-        <p className='text-accent text-center drop-shadow-2xl-red'>
-          Send a message to start the conversation
-        </p>
-      )}
+      <>
+        {!isLoading && messages.length > 0 && (
+          <ul className='flex-1 px-4 overflow-auto'>
+            {messages.map((message, index) => {
+              if (
+                message.receiver._id === selectedConversation._id ||
+                message.sender._id === selectedConversation._id
+              ) {
+                return (
+                  <li
+                    key={message._id}
+                    ref={(element) => {
+                      if (index === messages.length - 1) {
+                        setLastMessageElement(element);
+                      }
+                    }}
+                  >
+                    <Message message={message} />
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        )}
+        {!isLoading && messages.length === 0 && (
+          <p className='text-accent text-center drop-shadow-2xl-red'>
+            Send a message to start the conversation
+          </p>
+        )}
+        {!isLoading && <LoadMoreMessages />}
+      </>
     </div>
   );
 };
