51e62b27d3302cf9fff6c499ad23c2336ec76bbc
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-09 19:24:37 +0200

    style: update UI for skeleton

--- frontend/src/components/shared/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/shared/skeletons/MessageSkeleton.jsx
@@ -2,17 +2,17 @@ const MessageSkeleton = () => {
   return (
     <>
       <div className='flex gap-3 items-center'>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0'></div>
+        <div className='skeleton bg-primary/50 w-10 h-10 rounded-full shrink-0'></div>
         <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40'></div>
-          <div className='skeleton h-4 w-48'></div>
+          <div className='skeleton bg-primary/50 h-5 w-40'></div>
+          <div className='skeleton bg-primary/50 h-5 w-48'></div>
         </div>
       </div>
       <div className='flex gap-3 items-center justify-end'>
         <div className='flex flex-col gap-1'>
-          <div className='skeleton h-4 w-40'></div>
+          <div className='skeleton bg-primary/50 h-5 w-40'></div>
         </div>
-        <div className='skeleton w-10 h-10 rounded-full shrink-0'></div>
+        <div className='skeleton bg-primary/50 w-10 h-10 rounded-full shrink-0'></div>
       </div>
     </>
   );
