4d6575c71c8de5b057a59ccb2d879d7d4f59a1a4
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-10 00:59:55 +0200

    refactor: Implemented cleanup function for abort the fetch or ignore its result

--- frontend/src/shared/hooks/useGetMessages.jsx
+++ frontend/src/shared/hooks/useGetMessages.jsx
@@ -1,19 +1,18 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import toast from 'react-hot-toast';
 
 import useConversation from '../../store/useConversation';
 import { getUserConversationId } from '../utils';
 
 export const useGetMessages = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const {
-    messages,
+    setIsLoading,
     setMessages,
+    addMessages,
+    messages,
     selectedConversation,
     conversationId,
     setConversationId,
-    totalPages,
     currentPage,
     setTotalPages,
     setCurrentPage,
@@ -21,57 +20,62 @@ export const useGetMessages = () => {
     setLimit,
   } = useConversation();
 
-  const getLastConversationId = useCallback(() => {
-    return selectedConversation?._id;
-  }, [selectedConversation]);
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const lastConversationId = selectedConversation?._id;
+      if (!lastConversationId || lastConversationId === '') {
+        return;
+      }
+
+      const res = await fetch(
+        `/api/messages/${lastConversationId}?page=${currentPage}&limit=${limit}`
+      );
+      const data = await res.json();
+
+      if (data.error || data.message) {
+        throw new Error(data.error || data.message);
+      }
+
+      const conversationsId = getUserConversationId(data.messages, lastConversationId);
+      setConversationId(conversationsId);
+
+      setTotalPages(data.totalPages);
+      setCurrentPage(data.currentPage);
+      setLimit(data.limit);
+
+      if (data.messages.length > 0 && currentPage > 1) {
+        addMessages(data.messages);
+      } else {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [
+    setIsLoading,
+    selectedConversation?._id,
+    currentPage,
+    limit,
+    setConversationId,
+    setTotalPages,
+    setCurrentPage,
+    setLimit,
+    addMessages,
+    setMessages,
+  ]);
 
   useEffect(() => {
-    const getMessages = async () => {
-      setIsLoading(true);
-      try {
-        const lastConversationId = getLastConversationId();
-        if (!lastConversationId) return;
+    let isCancel = false;
 
-        const res = await fetch(
-          `/api/messages/${lastConversationId}?page=${currentPage}&limit=${limit}`
-        );
-        const data = await res.json();
-
-        if (data.error || data.message) {
-          throw new Error(data.error || data.message);
-        }
-
-        // getting the conversation ID for a particular user and setting that ID as the active conversation
-        const conversationsId = getUserConversationId(data, lastConversationId);
-        // Set the conversation ID
-        setConversationId(conversationsId);
-
-        // Update page data
-        setTotalPages(data.totalPages);
-        setCurrentPage(data.currentPage);
-        setLimit(data.limit);
-
-        // Add new messages to the store if there are any
-        if (data.messages.length > 0) {
-          setMessages(data.messages);
-        }
-      } catch (error) {
-        toast.error(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getMessages();
-  }, [selectedConversation, limit, currentPage]);
-
-  const loadMoreMessages = (newCurrentPage) => {
-    console.log('Loading more messages for page:', newCurrentPage);
-    // Load the next page of messages if there are more pages to load
-    if (newCurrentPage <= totalPages) {
-      setCurrentPage(newCurrentPage);
+    if (!isCancel && selectedConversation && selectedConversation?._id) {
+      fetchData();
     }
-  };
 
-  return { messages, isLoading, conversationId, loadMoreMessages, currentPage };
+    return () => (isCancel = true);
+  }, [fetchData, selectedConversation, selectedConversation?._id]);
+
+  return { messages, conversationId };
 };
