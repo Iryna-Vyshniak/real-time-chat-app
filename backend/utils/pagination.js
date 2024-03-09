bbb8992c3859979f83b38b4f3025e66c58d71bb2
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-09 19:23:32 +0200

    fix: fixed condition for limit

--- backend/utils/pagination.js
+++ backend/utils/pagination.js
@@ -3,7 +3,7 @@ export const pagination = (persistPage = 1, persistLimit = 6) => {
   const parseLimit = parseInt(persistLimit);
 
   const page = parsePage >= 1 ? parsePage : 1;
-  const limit = parseLimit > 1 && parseLimit < 6 ? parseLimit : 6;
+  const limit = parseLimit > 1 && parseLimit < 6 ? 6 : parseLimit;
   const skip = (page - 1) * limit;
 
   return { page, skip, limit };
