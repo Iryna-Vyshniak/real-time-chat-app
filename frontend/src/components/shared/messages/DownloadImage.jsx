204c733cbe3a385062829823984e084296de138d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-14 23:37:16 +0200

    feat: Implemented DownloadImage component

---
+++ frontend/src/components/shared/messages/DownloadImage.jsx
@@ -0,0 +1,19 @@
+import { downloadImage } from '../../../shared/utils';
+
+import Icon from '../../ui/Icon';
+
+const DownloadImage = ({ message }) => {
+  return (
+    <a
+      href={downloadImage(message.img)}
+      download
+      target='_blank'
+      rel='noopener noreferrer'
+      className='absolute bottom-1 right-1 flex items-center justify-center p-1 hover:bg-white/20 rounded-lg focus:ring-1 focus:ring-primary focus:outline-none'
+    >
+      <Icon src='#icon-download' style='w-4 h-4' />
+    </a>
+  );
+};
+
+export default DownloadImage;
