d938fcfb94fda99855c463f0555e3bc7099d3bbe
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-17 04:01:03 +0200

    feat: Implemented hook to useEmojiPicker for pick emoji

---
+++ frontend/src/shared/hooks/useEmojiPicker.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from 'react';
+
+import useConversation from '../../store/useConversation';
+
+const useEmojiPicker = () => {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [currentPopupId, setCurrentPopupId] = useState(null);
+
+  const {  addEmoji } = useConversation();
+  const emojiPickerRef = useRef(null);
+
+  // show popup
+  const openEmojiPicker = (id) => {
+    setShowEmojiPicker(true);
+    setCurrentPopupId(id);
+  };
+
+  // add emoji
+  const addEmojis = (messageId, emoji) => {
+    addEmoji(messageId, emoji);
+    setCurrentPopupId(null);
+    setShowEmojiPicker(false);
+  };
+
+  // remove emoji
+  const onEmojiClick = () => {
+    setShowEmojiPicker(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (emojiPickerRef.current && !emojiPickerRef.current.contains(event.target)) {
+        setCurrentPopupId(null);
+        setShowEmojiPicker(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return {
+    currentPopupId,
+    showEmojiPicker,
+    emojiPickerRef,
+    onEmojiClick,
+    addEmojis,
+    openEmojiPicker,
+  };
+};
+
+export default useEmojiPicker;
