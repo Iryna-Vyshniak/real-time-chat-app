fa85f5ecfbd08009c2c6eace9b5e79a408665ec9
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-17 22:29:20 +0200

    feat: Added Back Link with Gradient Effect

--- frontend/src/pages/update/UpdateProfilePage.jsx
+++ frontend/src/pages/update/UpdateProfilePage.jsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import useUpdateProfile from '../../shared/hooks/useUpdateProfile';
 
 import Title from '../../components/ui/Title';
 import UpdateForm from '../../components/ui/UpdateForm';
+import Icon from '../../components/ui/Icon';
 
 const UpdateProfilePage = () => {
   const { update, isLoading } = useUpdateProfile();
@@ -13,6 +16,17 @@ const UpdateProfilePage = () => {
 
   return (
     <div className='flex flex-col item-center justify-center mx-auto w-96 max-w-full'>
+      <Link
+        to='/'
+        className='group flex items-center ml-1 text-white transition-all duration-200 ease-in-out'
+      >
+        {' '}
+        <Icon src='#icon-chevrons-left' style='w-5 h-5' />
+        <span className='bg-left-bottom bg-gradient-to-r from-white to-primary bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out'>
+          {' '}
+          Back
+        </span>
+      </Link>
       <div className='p-6 w-full rounded-lg shadow-md bg-slate-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
         <Title text='Update Profile' span='ChatApp' />
 
