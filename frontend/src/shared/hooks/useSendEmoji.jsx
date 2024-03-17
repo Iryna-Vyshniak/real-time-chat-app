d0fc53e783afde94c8aeef0ce45aa2fc384276ee
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-17 04:00:18 +0200

    feat: Implemented hook useSendEmoji

---
+++ frontend/src/shared/hooks/useSendEmoji.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useState } from 'react';
+import useConversation from '../../store/useConversation';
+import toast from 'react-hot-toast';
+
+const useSendEmoji = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { addEmoji, selectedConversation } = useConversation();
+
+  const sendEmoji = useCallback(
+    async ({ messageId, emoji }) => {
+      if (!selectedConversation?._id) return;
+
+      setIsLoading(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation._id}/emoji/${messageId}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ emoji }),
+        });
+
+        const data = await res.json();
+
+        if (data.error || data.message) {
+          throw new Error(data.error || data.message);
+        }
+
+        addEmoji(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [addEmoji, selectedConversation._id]
+  );
+
+  return { isLoading, sendEmoji };
+};
+
+export default useSendEmoji;
