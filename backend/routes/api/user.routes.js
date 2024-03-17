ccea2439bb61ed53ebe55ae5e0564621652cd284
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-17 22:26:31 +0200

    feat: Implemented router for get user info

--- backend/routes/api/user.routes.js
+++ backend/routes/api/user.routes.js
@@ -6,6 +6,7 @@ import protectRoute from '../../middlewares/protectRoute.js';
 const router = Router();
 
 router.get('/', protectRoute, userCtrl.getUsersForSidebar);
+router.get('/:id', protectRoute, userCtrl.getUserInfo);
 router.patch('/messages', protectRoute, userCtrl.updateMessageStatus);
 router.put('/update/:id', protectRoute, userCtrl.updateUser);
 
