7fb2b6d533be395c47b6b7bde9985bdde4fc9fff
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-20 20:37:00 +0200

    feat: Implemented listen socket to delete message

--- frontend/src/shared/hooks/useListenMessages.jsx
+++ frontend/src/shared/hooks/useListenMessages.jsx
@@ -10,19 +10,32 @@ import notifySound from '../../assets/sounds/notify-sound.mp3';
 export const useListenMessages = () => {
   const { socket, onlineUsers } = useSocketContext();
 
-  const { addMessage, selectedConversation, notification, setNotification } = useConversation();
+  const {
+    addMessage,
+    selectedConversation,
+    notification,
+    setNotification,
+    deleteNotification,
+    deleteMessage,
+    messages,
+  } = useConversation();
 
   const handleNewMessage = useCallback(
     (newMessage) => {
       const sound = new Audio(messageSound);
       const notify = new Audio(notifySound);
-      const receiverIsOnline = onlineUsers.some((user) => user._id === newMessage.receiver._id);
+      // Check if receiver online or not
+      const receiverIsOnline = onlineUsers.some((user) => user === newMessage.receiver._id);
       // Check if conversation is selected
       const conversationNotSelected =
         !selectedConversation || selectedConversation?._id !== newMessage.sender._id;
 
-      if (!receiverIsOnline && conversationNotSelected) {
-        if (!notification.includes(newMessage._id)) {
+      // checking whether the recipient is different from the sender before adding a notification - help you avoid accidentally adding notifications for your own messages.
+      const isDifferentRecipient = newMessage.receiver._id !== newMessage.sender._id;
+      const hasNotification = notification.some((msg) => msg._id === newMessage._id);
+
+      if (receiverIsOnline && isDifferentRecipient && conversationNotSelected) {
+        if (!hasNotification) {
           setNotification([...notification, newMessage]);
           notify.play();
         }
@@ -35,21 +48,52 @@ export const useListenMessages = () => {
     [addMessage, notification, onlineUsers, selectedConversation, setNotification]
   );
 
+  const handleDeleteMessage = useCallback(
+    (id) => {
+      const senderId = messages
+        .filter((message) => message._id === id)
+        .map((message) => message.sender._id)
+        .join(' ');
+
+      // Check if conversation is selected
+      const conversationNotSelected =
+        !selectedConversation || selectedConversation?._id !== senderId;
+
+      if (conversationNotSelected) {
+        const findNotification = notification.find((n) => n._id === id);
+
+        if (findNotification) {
+          deleteNotification(id);
+          setNotification(notification.filter((n) => n._id !== id));
+        }
+      }
+      deleteMessage(id);
+    },
+    [
+      deleteMessage,
+      deleteNotification,
+      messages,
+      notification,
+      selectedConversation,
+      setNotification,
+    ]
+  );
+
   useEffect(() => {
-    const socketListener = (newMessage) => {
+    const addMessageListener = (newMessage) => {
       handleNewMessage(newMessage);
     };
 
-    socket?.on('newMessage', (newMessage) => socketListener(newMessage));
+    const deleteMessageListener = ({ id }) => {
+      handleDeleteMessage(id);
+    };
 
-    return () => socket?.off('newMessage');
-  }, [
-    addMessage,
-    handleNewMessage,
-    notification,
-    onlineUsers,
-    selectedConversation,
-    setNotification,
-    socket,
-  ]);
+    socket?.on('newMessage', addMessageListener);
+    socket?.on('deleteMessage', deleteMessageListener);
+
+    return () => {
+      socket?.off('newMessage');
+      socket?.off('deleteMessage');
+    };
+  }, [handleDeleteMessage, handleNewMessage, socket]);
 };
