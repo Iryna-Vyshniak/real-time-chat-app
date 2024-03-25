6df0a050cbc46c181e6ca492b1522f683186c6bc
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-26 00:11:30 +0200

    refactor(client): Renamed Header component for improved clarity and consistency

---
+++ frontend/src/components/shared/messages/MessagesBlockHeader.jsx
@@ -0,0 +1,10 @@
+const MessagesBlockHeader = ({ name }) => {
+  return (
+    <div className='px-4 py-2 mt-2 md:mt-0 mb-8 w-full h-9 bg-primary rounded-lg'>
+      <span className='text-[12px] sm:label-text text-slate-800'>To:</span>{' '}
+      <span className='text-slate-800 font-semibold tracking-widest'>{name}</span>
+    </div>
+  );
+};
+
+export default MessagesBlockHeader;
