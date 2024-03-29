a012c4de68f998d580fcefa87e43e67c50502467
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-29 22:09:43 +0200

    feat(client): Implemented AvatarGroup component

---
+++ frontend/src/components/ui/AvatarGroup.jsx
@@ -0,0 +1,22 @@
+const AvatarGroup = ({ avatars }) => {
+  return (
+    <ul className='avatar-group -space-x-[18px] rtl:space-x-reverse'>
+      {avatars.slice(0, 3).map((avatar) => (
+        <li key={avatar} className='avatar'>
+          <div className='w-8 h-8 rounded-full'>
+            <img src={avatar} className='w-10 h-10 rounded-full' />
+          </div>
+        </li>
+      ))}
+      {avatars.length > 3 && (
+        <div className='avatar placeholder'>
+          <div className='w-8 h-8 rounded-full bg-green text-slate-800 text-xs'>
+            <span>+{Number(avatars.length - 3)}</span>
+          </div>
+        </div>
+      )}
+    </ul>
+  );
+};
+
+export default AvatarGroup;
