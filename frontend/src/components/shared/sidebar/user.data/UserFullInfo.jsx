a30f93a4e433f4b0c697f8f96cac0df3221f1783
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-30 00:02:22 +0200

    feat(client): Implemented  LinkToBack component to UserFullInfo

--- frontend/src/components/shared/sidebar/user.data/UserFullInfo.jsx
+++ frontend/src/components/shared/sidebar/user.data/UserFullInfo.jsx
@@ -1,21 +1,15 @@
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useRef } from 'react';
 
-import Icon from '../../../ui/Icon';
+import { LinkToBack } from '../../../ui/LinkToBack';
 
 const UserFullInfo = ({ user }) => {
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/');
+
   return (
     <div className='p-4 w-[90%] md:w-1/3 h-auto bg-primary/10 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-1 rounded-md text-slate-300 text-lg drop-shadow-1xl-black'>
-      <Link
-        to='/'
-        className='group flex items-center ml-1 text-white transition-all duration-200 ease-in-out drop-shadow-2xl-black'
-      >
-        {' '}
-        <Icon src='#icon-chevrons-left' style='w-5 h-5' />
-        <span className='bg-left-bottom bg-gradient-to-r from-white to-primary bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out'>
-          {' '}
-          Back
-        </span>
-      </Link>
+      <LinkToBack to={backLinkLocationRef.current}>Back</LinkToBack>
       <div className='flex flex-col items-center justify-center gap-4'>
         {' '}
         <div className='w-full md:w-60 h-auto'>
