1a0a772226eb288a5543605340723359165207a4
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-30 00:05:55 +0200

    feat(client): Implemented FoundUsersList component

---
+++ frontend/src/components/shared/sidebar/user.data/FoundUsersList.jsx
@@ -0,0 +1,21 @@
+import UserSimpleInfo from './UserSimpleInfo';
+
+const FoundUsersList = ({ foundUsers, handleGroup }) => {
+  return (
+    <ul className='flex items-center gap-2 flex-wrap'>
+      {' '}
+      {foundUsers?.length > 0 &&
+        foundUsers.slice(0, 4).map((user) => (
+          <li
+            key={user._id}
+            className='w-[10rem] bg-green/10 hover:bg-green/20 rounded-lg shadow-md transition-all duration-200 ease-out'
+          >
+            {' '}
+            <UserSimpleInfo user={user} handleGroup={() => handleGroup(user)} />
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+export default FoundUsersList;
