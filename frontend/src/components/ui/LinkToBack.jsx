0ea9b4bfa0d0081b78eff9308f859b08c48f7240
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-29 22:08:28 +0200

    feat(client): Implemented LinkToBack component

---
+++ frontend/src/components/ui/LinkToBack.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import Icon from './Icon';
+
+export const LinkToBack = ({ children, to }) => {
+  return (
+    <Link
+      to={to}
+      className='group flex items-center ml-1 text-white transition-all duration-200 ease-in-out drop-shadow-2xl-black'
+    >
+      <Icon src='#icon-chevrons-left' style='w-5 h-5' />
+      <span className='bg-left-bottom bg-gradient-to-r from-white to-primary bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out'>
+        {children}
+      </span>
+    </Link>
+  );
+};
