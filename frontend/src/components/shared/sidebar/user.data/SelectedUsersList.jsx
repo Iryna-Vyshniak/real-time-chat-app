accc388a6644f9ea59cd1d28ab49b191a15fe1d5
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-03-30 00:06:18 +0200

    feat(client): Implemented SelectedUsersList component

---
+++ frontend/src/components/shared/sidebar/user.data/SelectedUsersList.jsx
@@ -0,0 +1,21 @@
+import UserSimpleInfo from './UserSimpleInfo';
+
+const SelectedUsersList = ({ selectedUsers, handleDelete }) => {
+  return (
+    <ul className='flex items-center justify-center gap-2 flex-wrap'>
+      {' '}
+      {selectedUsers?.length > 0 &&
+        selectedUsers.map((user) => (
+          <li
+            key={user._id}
+            className='w-[12rem] bg-primary hover:bg-green text-slate-800 rounded-lg shadow-md transition-all duration-200 ease-out'
+          >
+            {' '}
+            <UserSimpleInfo user={user} handleGroup={() => handleDelete(user)} close={true} />
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+export default SelectedUsersList;
