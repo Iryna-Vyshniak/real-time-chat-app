64139046d5151a48021713d3f488b961860123fe
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-11 23:14:11 +0300

    refactored(client): Refactor UserSimpleInfo for Admin-Specific Close Button - Add `adminName` prop to `UserSimpleInfo` component. - Modify the condition for rendering the close button to check if `adminName.fullName` is not equal to `user.fullName`. - This change ensures that the close button is only shown for users who are not the admin, enhancing the admin's control over the user interface.

--- frontend/src/components/features/sidebar/user.data/UserSimpleInfo.jsx
+++ frontend/src/components/features/sidebar/user.data/UserSimpleInfo.jsx
@@ -1,6 +1,6 @@
 import Avatar from '../../../ui/Avatar';
 
-const UserSimpleInfo = ({ handleGroup, user, close }) => {
+const UserSimpleInfo = ({ handleGroup, user, adminName, close }) => {
   return (
     <div className='relative flex items-center gap-1 px-2 py-1' onClick={handleGroup}>
       <Avatar src={user.avatar} />
@@ -8,7 +8,7 @@ const UserSimpleInfo = ({ handleGroup, user, close }) => {
         <h1 className='text-xs '>{user.fullName}</h1>
         <p className='text-xs'>@{user.username}</p>
       </div>
-      {close && (
+      {close && adminName.fullName !== user.fullName && (
         <button className='btn btn-xs btn-circle btn-ghost absolute right-0 top-0 text-[8px]'>
           ✕
         </button>
