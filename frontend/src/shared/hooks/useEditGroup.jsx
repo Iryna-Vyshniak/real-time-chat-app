d1b43770695a9d86c89f0e3980fc4b0a176f2a02
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-12 03:36:54 +0300

    feat(client): Implemented hook for editing group chat details

---
+++ frontend/src/shared/hooks/useEditGroup.jsx
@@ -0,0 +1,44 @@
+import { useCallback, useState } from 'react';
+import toast from 'react-hot-toast';
+
+import useConversation from '../../store/useConversation';
+
+export const useEditGroup = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { selectedConversation, groups, setGroups } = useConversation();
+
+  const editGroup = useCallback(
+    async ({ chatname, chatAvatar, users }) => {
+      if (!selectedConversation?.data?._id) return;
+
+      const groupId = selectedConversation?.data?._id;
+
+      setIsLoading(true);
+      try {
+        const res = await fetch(`/api/chat/group-edit/${groupId}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ chatname, chatAvatar, users }),
+        });
+
+        const data = await res.json();
+
+        if (data.error || data.message) {
+          throw new Error(data.error || data.message);
+        }
+        const updatedGroups = groups.map((group) => (group._id === data._id ? data : group));
+        console.log('updatedGroups: ', updatedGroups);
+        setGroups(updatedGroups);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [groups, selectedConversation?.data?._id, setGroups]
+  );
+
+  return { isLoading, editGroup };
+};
