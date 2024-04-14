3ed44255a345f52119190bf8d6725609d33fef6e
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-14 17:00:49 +0300

    refactor(client): Enhanced user selection in chat - Updated the user selection logic in the Notification component. - Now, when a user is selected, not only is the selectedConversation state updated with the user's details (including new fields: gender and createdAt), but the notification state is also updated. - The notification state now filters out any notifications from the selected user, effectively marking their messages as read upon selection.

--- frontend/src/components/features/navbar/Notification.jsx
+++ frontend/src/components/features/navbar/Notification.jsx
@@ -30,27 +30,32 @@ const Notification = () => {
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-8 p-1 rounded-box w-52 shadow-md bg-primary'
             >
-              {uniqueSenders.map(({ sender: { _id, fullName, username, avatar }, count }, idx) => (
-                <li
-                  key={_id + idx}
-                  onClick={() => {
-                    setSelectedConversation({
-                      type: 'private',
-                      data: { _id, fullName, username, avatar },
-                    });
-                    setNotification(
-                      notification.filter(({ sender: { _id: idSender } }) => idSender !== _id)
-                    );
-                  }}
-                  className='grid grid-cols-[1fr,40px] gap-2 items-center justify-between text-slate-800'
-                >
-                  <p className='text-[10px]'>{`New message from ${username}`} </p>
-                  <span className='flex items-center justify-center shadow bg-secondary h-4 w-4 text-[10px] rounded-full text-slate-800'>
-                    {' '}
-                    {`${count}`}
-                  </span>
-                </li>
-              ))}
+              {uniqueSenders.map(
+                (
+                  { sender: { _id, fullName, username, avatar, gender, createdAt }, count },
+                  idx
+                ) => (
+                  <li
+                    key={_id + idx}
+                    onClick={() => {
+                      setSelectedConversation({
+                        type: 'private',
+                        data: { _id, fullName, username, avatar, gender, createdAt },
+                      });
+                      setNotification(
+                        notification.filter(({ sender: { _id: idSender } }) => idSender !== _id)
+                      );
+                    }}
+                    className='grid grid-cols-[1fr,40px] gap-2 items-center justify-between text-slate-800'
+                  >
+                    <p className='text-[10px]'>{`New message from ${username}`} </p>
+                    <span className='flex items-center justify-center shadow bg-secondary h-4 w-4 text-[10px] rounded-full text-slate-800'>
+                      {' '}
+                      {`${count}`}
+                    </span>
+                  </li>
+                )
+              )}
             </ul>
             {(notification.length > 0 || lastMessages.length > 0) && (
               <span className='absolute top-0 right-0 flex items-center justify-center w-[16px] h-[16px] rounded-full bg-primary indicator-item text-slate-800 text-[7px] drop-shadow-5xl-black'>
