135bfb886f57f47f6a181f463633e95b6870db2d
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-21 21:14:30 +0300

    refactor(client): Refactored navigation link component and profile update page

--- frontend/src/pages/update/UpdateProfilePage.jsx
+++ frontend/src/pages/update/UpdateProfilePage.jsx
@@ -1,12 +1,15 @@
-import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import useUpdateProfile from '../../shared/hooks/useUpdateProfile';
 
 import Title from '../../components/ui/Title';
 import UpdateForm from '../../components/ui/UpdateForm';
-import Icon from '../../components/ui/Icon';
+import { LinkToBack } from '../../components/ui/LinkToBack';
 
 const UpdateProfilePage = () => {
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/');
   const { update, isLoading } = useUpdateProfile();
 
   const onSubmit = async (state) => {
@@ -16,17 +19,7 @@ const UpdateProfilePage = () => {
 
   return (
     <div className='flex flex-col item-center justify-center mx-auto w-96 max-w-full'>
-      <Link
-        to='/'
-        className='group flex items-center ml-1 text-white transition-all duration-200 ease-in-out'
-      >
-        {' '}
-        <Icon src='#icon-chevrons-left' style='w-5 h-5' />
-        <span className='bg-left-bottom bg-gradient-to-r from-white to-primary bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out'>
-          {' '}
-          Back
-        </span>
-      </Link>
+      <LinkToBack to={backLinkLocationRef.current}>Back</LinkToBack>
       <div className='p-6 w-full rounded-lg shadow-md bg-slate-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
         <Title text='Update Profile' span='ChatApp' />
 
