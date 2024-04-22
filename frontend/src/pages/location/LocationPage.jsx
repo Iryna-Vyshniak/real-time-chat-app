c3e1b7b8a50bc5368d66074a1704558d6dd7d49b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-23 00:40:46 +0300

    feat(client): Implemented LocationPage component

---
+++ frontend/src/pages/location/LocationPage.jsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { useListenLocation } from '../../shared/hooks/useListenLocation';
+
+import Map from './location.content/Map';
+import Status from './location.content/Status';
+import ShareLocation from './location.content/ShareLocation';
+
+import { LinkToBack } from '../../components/ui/LinkToBack';
+
+const LocationPage = () => {
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? '/');
+  const { position, locationStatus } = useListenLocation();
+
+  return (
+    <section className='flex flex-col gap-4 w-1/2'>
+      <LinkToBack to={backLinkLocationRef.current}>Back</LinkToBack>
+      <Status position={position} locationStatus={locationStatus} />
+      <ShareLocation position={position} locationStatus={locationStatus} />
+      {position ? (
+        <Map position={position} />
+      ) : (
+        <div className='flex items-center justify-center'>
+          <p className='loading loading-ring loading-lg'></p>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default LocationPage;
