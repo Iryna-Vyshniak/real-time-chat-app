d5664499d83a19fb48a2e375e663612af796d3dd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-04-27 22:48:52 +0300

    feat(server): Added delete group route This route is protected and only authenticated users can access it. When this route is hit, the deleteGroup function from chat.controller.js is called to handle the request

--- backend/routes/api/chat.routes.js
+++ backend/routes/api/chat.routes.js
@@ -9,5 +9,6 @@ router.post('/group', protectRoute, chatCtrl.createGroupChat);
 router.post('/group/:groupId/pin-group', protectRoute, chatCtrl.pinGroupChat);
 router.put('/group-edit/:groupId', protectRoute, chatCtrl.updateGroupChat);
 router.delete('/group/:groupId/delete-user', protectRoute, chatCtrl.deleteUserFromGroup);
+router.delete('/group/:groupId', protectRoute, chatCtrl.deleteGroup);
 
 export default router;
